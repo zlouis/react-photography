02f0d9e39bc20a35fe8e8c260323115b103edc9f
zlouis <jiannchyao@gmail.com>
2017-02-17 17:44:47 -0500

    working navbar, implemented react-boostrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import styles from './Header.css';
+import { Navbar, Nav, NavItem } from 'react-bootstrap'; 
 
-var Header = React.createClass({
+const Header = React.createClass({
   render: function() {
     return(
-  <div>
-          <nav className="navbar">
-           <div className="container-fluid"> 
-            <div className="headerLinks">
-              <a href="index.html" className={styles.portstyle} > S & L </a>
-              <a href="adventure.html" className={styles.travelstyle}> Adventure </a> 
-              <a href="www.google.com" className={styles.kitchenstyle}> Culinary Arts </a> 
-              <a href="about.html" className={styles.contactstyle}> About</a> 
-            </div>
-           </div>
-          </nav>
+<div>
+  <Navbar className={styles.navbarfont }>
+     <Navbar.Header>
+       <Navbar.Brand>
+         <a href="index.html" className={styles.portstyle}>S & L</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="adventure.html" className={styles.travelstyle}>Adventure</NavItem>
+        <NavItem eventKey={2} href="www.google.com" className={styles.kitchenstyle}>Culinary Arts</NavItem>
+       <NavItem eventKey={3} href="about.html" className={styles.contactstyle} >About</NavItem>
+     </Nav>
+   </Navbar>
   </div>
   );
   }
   });
+
+
 module.exports = Header;
+
+
+
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Counter from './Counter';
 import Header from './components/Header';
-// import styles from
+
 
  
 // document.addEventListener('DOMContentLoaded', function() {
