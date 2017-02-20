acc06bb6b608271eca7d5c79cbc96af207cb3399
zlouis <jiannchyao@gmail.com>
2017-02-20 16:21:55 -0500

    added mongoose and mongod connection, included morgan and bodyparser

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const app = express();
- 
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const compiler = webpack(webpackConfig);
- 
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 app.use(express.static(__dirname + '/www'));
  
 app.use(webpackDevMiddleware(compiler, {
@@ -17,9 +25,22 @@ app.use(webpackDevMiddleware(compiler, {
   },
   historyApiFallback: true,
 }));
- 
+
+//connecting to MongoDB
+mongoose.connect('mongodb://localhost/react-photography');
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+  console.log('Mongoose Error: ' + err);
+});
+
+db.once('open', function(){
+  console.log("connected to Mongoose!")
+});
+
+
 const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('listening on', host, port);
 });
