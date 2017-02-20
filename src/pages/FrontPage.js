f6d353715ae2800b641af541c903d0738c63d083
zlouis <jiannchyao@gmail.com>
2017-02-20 17:49:15 -0500

    map files added

---
+++ src/pages/FrontPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Grid, Row, Col } from 'react-bootstrap';
+import styles from './Header.css';
+
+
+const Front = React.createClass({
+  render: function() {
+    return(
+      <div>
+
+      )
+  }
+})
