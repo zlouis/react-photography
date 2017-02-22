ea096ed3716ae7234c0bf5d523a2cdd037a1826e
zlouis <jiannchyao@gmail.com>
2017-02-21 19:18:31 -0500

    drop mongodb & mongoose connection, added dropbox api

---
+++ dropboxAPI.js
@@ -0,0 +1,13 @@
+// var Dropbox = require('dropbox');
+
+
+// var dbx = new Dropbox({ accessToken: 'oplxrfrn4bquv6n' });
+// dbx.filesListFolder({path: 'home/MoMa_Apr2016'})
+//   .then(function(response) {
+//     console.log(response);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+//   export.modules.dbx
--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const Dropbox = require('dropbox');
 const compiler = webpack(webpackConfig);
+
+
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -38,9 +42,22 @@ db.once('open', function(){
   console.log("connected to Mongoose!")
 });
 
+//dropbox connection
+var dbx = new Dropbox({ accessToken: 'fSYqugPOt-IAAAAAAAAEEbxq1UkI2CdnuCBQjKAtypjcm8nyn8nE7U_FcqtdFBMH' });
+dbx.filesListFolder({path: '/MoMa_Apr2016'})
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+
 
 const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('listening on', host, port);
 });
+
+
