e088e41b4e0c4691ba5616f6e9bdf31d20ba6e2d
zlouis <jiannchyao@gmail.com>
2017-03-24 17:01:29 -0400

    route testing

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 // import Counter from './Counter';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import Header from './components/Header';
 import FrontPage from './pages/FrontPage';
 
 
  
-// document.addEventListener('DOMContentLoaded', function() {
-//   ReactDOM.render(
-//     React.createElement(Counter),
-//     document.getElementById('mount')
+
+//working code below
+// ReactDOM.render(
+// <div>
+//   <Header />
+
+//   <FrontPage />
+
+
+// </div> , document.getElementById("main")
 //   );
-// });
 
-ReactDOM.render(
-<div>
-  <Header />
+render(
+  <Router>
+      <Route path='/' component={Header} history={browserHistory}>
+       <Route path='/frontpage' component= {FrontPage}/>
+      </Route>
+  </Router>,  document.getElementById('main')
 
-  <FrontPage />
 
-
-</div> , document.getElementById("main")
-  );
+  )
