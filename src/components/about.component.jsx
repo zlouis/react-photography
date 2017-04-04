e7f414004733b81bdc5b7ac9130be46102c247b3
zlouis <jiannchyao@gmail.com>
2017-04-04 18:17:38 -0400

    fixed height and width

--- src/components/about.component.jsx
+++ src/components/about.component.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import styles from './Header.css';
 import { Navbar, Nav, NavItem } from 'react-bootstrap'; 
 
-const Main = React.createClass({
-  render: function() {
-    return(
-<div>
-  <Navbar className={styles.navbarfont }>
-     <Navbar.Header>
-       <Navbar.Brand>
-         <a href="index.html" className={styles.portstyle}>S & L</a>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav>
-        <NavItem eventKey={1} href="adventure.html" className={styles.travelstyle}>Adventure</NavItem>
-        <NavItem eventKey={2} href="www.google.com" className={styles.kitchenstyle}>Culinary Arts</NavItem>
-       <NavItem eventKey={3} href="about.html" className={styles.contactstyle} >About</NavItem>
-     </Nav>
-   </Navbar>
-  </div>
-  );
-  }
-  });
+const About =() => {
+  return(
+    <div>
+      <p> About </p>
+
+          </div>
+    )
+}
+
+export default About;
 
 
-module.exports = Main;
 
--- src/components/main.component.jsx
+++ src/components/main.component.jsx
@@ -8,7 +8,7 @@ import Home from './home.component.jsx';
 import Adventure from './adventure.component.jsx';
 import { Navbar, Nav, NavItem, Carousel,Grid, Row, Col } from 'react-bootstrap'; 
 import styles from './css/Header.css';
-
+import About from './about.component.jsx';
 
 
 
@@ -28,47 +28,48 @@ const Main =() => {
     <Navbar.Collapse>
       <Nav>
         <NavItem eventKey={1} href="#"><Link to='/adventure' className={[styles.navAdventure], [styles.headerLinks]}> Adventure</Link></NavItem>
-        <NavItem eventKey={2} href="#"><Link to='/kitchen' className={styles.headerLinks}> About</Link></NavItem>
+        <NavItem eventKey={2} href="#"><Link to='/about' className={styles.headerLinks}> About</Link></NavItem>
           <hr/>
             <Router exactly pattern='/' component={Home}/>
             <Router pattern='/adventure' component={Adventure}/>
-            <Router pattern='/about' component={Home}/> 
+            <Router pattern='/about' component={About}/> 
       </Nav>
        </Navbar.Collapse>
   </Navbar>
-    <Row>
-      <Col sm={6} md={6} >
+    <Grid>
+    <Row className>
+      <Col md={6} md={12} className={styles.carouselPosition} >
         <Carousel>
     <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7d0s1N0NRZllwakE"/>
+      <img  alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7d0s1N0NRZllwakE"/>
       <Carousel.Caption>
         <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <p>Nulla vitae elit libero, a pharetra augue moll is interdum.</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7MVAySV9jME9TWWM"/>
+      <img src="https://docs.google.com/uc?id=0B0huBtqYaof7MVAySV9jME9TWWM"/>
       <Carousel.Caption>
         <h3>Second slide label</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7N250S3JicmxxNmM"/>
+      <img  src="https://docs.google.com/uc?id=0B0huBtqYaof7N250S3JicmxxNmM"/>
       <Carousel.Caption>
         <h3>Third slide label</h3>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
       </Carousel.Caption>
     </Carousel.Item>
         <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7MDF5Vm1VYWhoY3M"/>
+      <img  src="https://docs.google.com/uc?id=0B0huBtqYaof7MDF5Vm1VYWhoY3M"/>
       <Carousel.Caption>
         <h3>Third slide label</h3>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
       </Carousel.Caption>
     </Carousel.Item>
         <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7X1ROMUxRMjVsN00"/>
+      <img src="https://docs.google.com/uc?id=0B0huBtqYaof7X1ROMUxRMjVsN00"/>
       <Carousel.Caption>
         <h3>Third slide label</h3>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
@@ -77,6 +78,7 @@ const Main =() => {
   </Carousel>
 </Col>
 </Row>
+</Grid>
 
 
 
