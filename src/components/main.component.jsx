c7ef406adffc6df50dfb786587e12d9a3796825d
zlouis <jiannchyao@gmail.com>
2017-04-03 20:57:30 -0400

    added carousel

--- src/components/main.component.jsx
+++ src/components/main.component.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Link, Route, Miss } from 'react-router-dom';
 import FrontPage from '../pages/FrontPage.component.jsx';
 import Home from './home.component.jsx';
 import Adventure from './adventure.component.jsx';
-import { Navbar, Nav, NavItem, Carousel } from 'react-bootstrap'; 
+import { Navbar, Nav, NavItem, Carousel,Grid, Row, Col } from 'react-bootstrap'; 
 import styles from './css/Header.css';
 
 
@@ -36,7 +36,8 @@ const Main =() => {
       </Nav>
        </Navbar.Collapse>
   </Navbar>
-
+    <Row>
+      <Col sm={6} md={6} >
         <Carousel>
     <Carousel.Item>
       <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7d0s1N0NRZllwakE"/>
@@ -46,20 +47,37 @@ const Main =() => {
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
+      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7MVAySV9jME9TWWM"/>
       <Carousel.Caption>
         <h3>Second slide label</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
+      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7N250S3JicmxxNmM"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+        <Carousel.Item>
+      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7MDF5Vm1VYWhoY3M"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+        <Carousel.Item>
+      <img width={900} height={500} alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7X1ROMUxRMjVsN00"/>
       <Carousel.Caption>
         <h3>Third slide label</h3>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
       </Carousel.Caption>
     </Carousel.Item>
   </Carousel>
+</Col>
+</Row>
+
 
 
       
