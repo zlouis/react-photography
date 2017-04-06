70197479e1df407f40cabb29d8ad72a0431ab3c1
zlouis <jiannchyao@gmail.com>
2017-04-06 16:12:52 -0400

    fixed routing map

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import { Navbar, Nav, NavItem, Carousel,Grid, Row, Col } from 'react-bootstrap'; 
+import styles from './css/Header.css';
+
 
 const Home =() => {
   return(
-           (<h1>photography</h1>)
+    <div>
+           <Grid>
+    <Row >
+      <Col md={6} md={12} className={styles.carouselPosition} >
+        <Carousel>
+    <Carousel.Item>
+      <img  alt="900x500" src="https://docs.google.com/uc?id=0B0huBtqYaof7d0s1N0NRZllwakE"/>
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue moll is interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img src="https://docs.google.com/uc?id=0B0huBtqYaof7MVAySV9jME9TWWM"/>
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img  src="https://docs.google.com/uc?id=0B0huBtqYaof7N250S3JicmxxNmM"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+        <Carousel.Item>
+      <img  src="https://docs.google.com/uc?id=0B0huBtqYaof7MDF5Vm1VYWhoY3M"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+        <Carousel.Item>
+      <img src="https://docs.google.com/uc?id=0B0huBtqYaof7X1ROMUxRMjVsN00"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+</Col>
+</Row>
+</Grid>
+</div>
     )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 // Import routing components
-import { BrowserRouter as Router, Link, Match, Miss } from 'react-router';
+import { BrowserRouter as Router, Link, Route, Miss } from 'react-router-dom';
+import FrontPage from './pages/FrontPage.component.jsx';
+import Home from './components/home.component.jsx';
+import Adventure from './components/adventure.component.jsx';
+import { Navbar, Nav, NavItem, Carousel,Grid, Row, Col } from 'react-bootstrap'; 
+import styles from './components/css/Header.css';
+import About from './components/about.component.jsx';
 
 import Main from './components/main.component.jsx';
 
 ReactDOM.render(
-    <Main />,
+       <Router>
+      <div>
+
+<Navbar  className={styles.navBackground}>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to='/'> S & L </Link> 
+      </Navbar.Brand>
+      <Navbar.Toggle/>
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem><Link to='/FrontPage' className={[styles.navAdventure], [styles.headerLinks]}> Adventure</Link></NavItem>
+        <NavItem><Link to='/about' className={styles.headerLinks}> About</Link></NavItem>
+      </Nav>
+       </Navbar.Collapse>
+  </Navbar>
+         
+            <Route exact path='/' component={Home}/>
+            <Route path='/frontpage' component={FrontPage}/>
+            <Route path='/about' component={About}/> 
+  </div>
+
+  </Router>,
         document.getElementById("contents")
     )
