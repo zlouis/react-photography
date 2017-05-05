f594927918fda0b9e758f7472afeefaf0b4e7def
zlouis <jiannchyao@gmail.com>
2017-05-05 14:49:02 -0400

    changed import

--- src/pages/FrontPage.component.jsx
+++ src/pages/FrontPage.component.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 // import styles from './Header.css';
 import dropboxapi from '../utility/dropboxapi'
-import Masonry from 'react-masonry-component';
 import FingerLakes from './Albumset/FingerLakes.album.jsx';
 import Album from './album.component.jsx';
 import { BrowserRouter as Router, Link, Route, Miss } from 'react-router-dom';
@@ -53,9 +52,8 @@ const elements = [
         <Route path='/frontpage/Album' component={Album}/>
         <Route path='/frontpage/acadia' component={Acadia}/>
         </div>
-
-        <Row className={styles.albumBreak}>
-          <Col md={3} md={3} className={styles.container}>
+          <Row className={styles.albumBreak}>
+            <Col md={3} md={3} className={styles.container}>
         <div className={styles.textContainer}>
            <Scrollchor to="top"><Link to='/frontpage/FingerLakes'><img src="https://docs.google.com/uc?id=0B0huBtqYaof7Y1VPdmNEZzFQNkE"  className={styles.album} /></Link></Scrollchor>
             <div >
@@ -63,6 +61,7 @@ const elements = [
             </div>
         </div>
           </Col>
+          
           <Col md={3} md={3} className={styles.container}>
         <div className={styles.textContainer}> 
           <Scrollchor to="top"><Link to='/frontpage/album'><img src="https://docs.google.com/uc?id=0B0huBtqYaof7RV9GNS1uMENmZ3M"  className={styles.album} /></Link></Scrollchor>
